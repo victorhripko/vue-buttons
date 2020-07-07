9fa50dc0dd447085bfeb4e63879711d139c8d26b
Victor Hripko <hripko.v@soft2bet.com>
2020-07-07 17:13:13 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <h4 class="component-title">Colors</h4>
+    <Button color="default">color="default"</Button>
+    <Button color="primary">color="primary"</Button>
+    <Button color="secondary">color="secondary"</Button>
+    <Button color="danger">color="danger"</Button>
+
+    <h4 class="component-title">Sizes</h4>
+    <Button size="sm">size="sm"</Button>
+    <Button size="md">size="md"</Button>
+    <Button size="lg">size="lg"</Button>
+
+    <h4 class="component-title">Variants</h4>
+    <Button variant="text">variant="text"</Button>
+    <Button variant="outline">variant="outline"</Button>
+
+    <h4 class="component-title">Disabled</h4>
+    <Button disabled>disabled</Button>
+
+    <h4 class="component-title">Icons</h4>
+    <Button startIcon="4k" endIcon="power_settings_new">Hello</Button>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import Button from '@/components/Button.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    Button,
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    background-color: white;
+    color: black;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI",
+                  Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji",
+                  "Segoe UI Emoji", "Segoe UI Symbol";
+  }
+
+  .component-title {
+    font-size: 2em;
+  }
 </style>
---
+++ src/components/Button.vue
@@ -0,0 +1,64 @@
+<script>
+import './_Button.scss';
+
+export default {
+  name: 'Button',
+  props: {
+    variant: {
+      type: String,
+      validator(value) {
+        return ['outline', 'text'].indexOf(value) !== -1;
+      },
+    },
+    disableShadow: {
+      type: Boolean,
+      default: false,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    size: {
+      type: String,
+      validator(value) {
+        return ['sm', 'md', 'lg'].indexOf(value) !== -1;
+      },
+    },
+    color: {
+      type: String,
+      validator(value) {
+        return ['default', 'primary', 'secondary', 'danger'].includes(value);
+      },
+    },
+    startIcon: {
+      type: String,
+    },
+    endIcon: {
+      type: String,
+    },
+  },
+};
+</script>
+
+<template>
+  <button
+    class="btn"
+    :class="{
+      [`btn--${color}`]: color,
+      [`btn--${size}`]: size,
+      [`btn--${variant}`]: variant,
+      'btn--disable-shadow': disableShadow
+    }"
+    :disabled="disabled ? disabled : null"
+    :tabindex="disabled ? -1 : null"
+    type="button"
+  >
+    <i v-if="startIcon" class="material-icons material-icons--start">
+      {{startIcon}}
+    </i>
+    <slot />
+    <i v-if="endIcon" class="material-icons material-icons--end">
+      {{endIcon}}
+    </i>
+  </button>
+</template>
